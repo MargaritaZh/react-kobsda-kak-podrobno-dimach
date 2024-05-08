a4d0b3af0817c16700940e46fc00b4714d608c67
MargaritaZh <margaritazhitkevich@gmail.com>
2024-05-08 15:16:26 +0300

    add to idea ignore

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+    collapsed: boolean
+}
+
+export function Accordion(props: AccordionPropsType) {
+    if (props.collapsed === true) {
+        return (
+            <div>
+                <AccordionTitle title={props.titleValue}/>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <AccordionTitle title={props.titleValue}/>
+                <AccordionBody/>
+            </div>
+        )
+    }
+}
+
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    return (
+        <h3>{props.title}</h3>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
