a0f906a8e170af8399a55e3a8814595da979c7c3
MargaritaZh <margaritazhitkevich@gmail.com>
2024-05-16 23:47:14 +0300

    useState (OnOff,UncontrolledAccordion,UncontrolledRating)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import './App.css'
-import {Rating} from "./components/Rating/Rating";
-import {Accordion} from "./components/Accordion/Accordion";
+import OnOff from "./components/OnOff/OnOff";
+import {UncontrolledAccordion} from "./components/UncontroledAccordion/UncontrolledAccordion";
+import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
 
 
 function App() {
-  return (
-      <div>
-        <PageTitle title={"This is App component"}/>
-        <PageTitle title={"My friends"}/>
-        <Rating value={3}/>
-        <Accordion titleValue={"Menu"} collapsed={true}/>
-        <Accordion titleValue={"Users"} collapsed={false}/>
-        <Rating value={0}/>
-        <Rating value={1}/>
-        <Rating value={2}/>
-        <Rating value={3}/>
-        <Rating value={4}/>
-        <Rating value={5}/>
-      </div>
-  );
+    console.log("App rendering")
+    return (
+        <div>
+
+            <OnOff/>
+            <OnOff/>
+
+            <UncontrolledAccordion titleValue={"Menu"}/>
+            <UncontrolledAccordion titleValue={"Users"}/>
+
+            <UncontrolledRating />
+
+
+            {/*<PageTitle title={"This is App component"}/>*/}
+            {/*<PageTitle title={"My friends"}/>*/}
+
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+        </div>
+    );
 }
 
 type PageTitlePropsType = {
-  title: string
+    title: string
 }
 
 function PageTitle(props: PageTitlePropsType) {
-  return (
-      <h1>{props.title}</h1>
-  )
+    return (
+        <h1>{props.title}</h1>
+    )
 }
 
 export default App;
