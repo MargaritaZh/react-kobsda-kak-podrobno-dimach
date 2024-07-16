70b140324bc3717a96f19c64fabd1f55b5d598ee
MargaritaZh <margaritazhitkevich@gmail.com>
2024-07-16 23:32:55 +0300

    lesson Storybook- (input -ControlledSelect)

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -42,7 +42,11 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
 export const ControlledInput = () => {
     const [parentValue, setParentValue] = useState("")
 
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.value)
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+
+
     return <input value={parentValue} onChange={onChange}/>
 }
 
@@ -50,7 +54,9 @@ export const ControlledCheckbox = () => {
 
     const [parentValue, setParentValue] = useState(true)
 
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.checked)
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
 
     return <input type={"checkbox"} checked={parentValue}
                   onChange={onChange}/>
@@ -58,6 +64,16 @@ export const ControlledCheckbox = () => {
 
 
 export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined)
 
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <select value={parentValue} onChange={onChange}>
+        <option>none</option>
+        <option value={"1"}>Minsk</option>
+        <option value={"2"}>Kiev</option>
+        <option value={"3"}>Moscow</option>
+    </select>
 
 }
