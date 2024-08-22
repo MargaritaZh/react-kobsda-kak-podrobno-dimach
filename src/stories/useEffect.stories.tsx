a63ad01539b2667383680788fc346e454e6dfa0e
MargaritaZh <margaritazhitkevich@gmail.com>
2024-08-22 15:16:10 +0300

    lesson - useEffect ,setTimeout,setInterval

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -6,7 +6,7 @@ export default {
 
 export const SimpleExample = () => {
 
-   const [fake,setFake]=useState(1)
+    const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
 
     console.log("SimpleExample")
@@ -14,7 +14,7 @@ export const SimpleExample = () => {
     useEffect(() => {
         debugger
         console.log("useEffect every render component")
-        document.title=counter.toString()
+        document.title = counter.toString()
         //api.getUsers().them("")
         //setInterval
         //indexedDB
@@ -25,30 +25,66 @@ export const SimpleExample = () => {
     useEffect(() => {
         debugger
         console.log("useEffect only first render")
-        document.title=counter.toString()
+        document.title = counter.toString()
 
         //api.getUsers().them("")
         //setInterval
         //indexedDB
         //document.getElementId
         //document.title="User"
-    },[])
+    }, [])
 
     useEffect(() => {
         debugger
         console.log("useEffect first render and every counter change")
-        document.title=counter.toString()
+        document.title = counter.toString()
 
         //api.getUsers().them("")
         //setInterval
         //indexedDB
         //document.getElementId
         //document.title="User"
-    },[counter])
+    }, [counter])
 
-    return<>
+    return <>
         Hello,{counter} {fake}
-        <button onClick={()=>setFake(fake+1)}>+</button>
-        </>
+        <button onClick={() => setFake(fake + 1)}>+</button>
+    </>
 }
 
+
+export const SetTimeoutExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log("SimpleExample")
+
+    //ПОМЕНЯЙ TITLE ВСЕГО ОДИН РАЗ,КОГДА КОМПАНЕНТА ВМОНТИРОВАЛАСЬ
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         console.log("SetTimeOut")
+    //         document.title = counter.toString()
+    //     }, 1000)
+    // }, [])
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         console.log("SetTimeOut")
+    //         document.title = counter.toString()
+    //     }, 1000)
+    // }, [counter])
+
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(state=>state+1)
+        }, 1000)
+    }, [])
+
+    return <>
+        Hello,counter:{counter}, fake: {fake}
+
+        {/*<button onClick={() => setFake(fake + 1)}>+</button>*/}
+    </>
+}
